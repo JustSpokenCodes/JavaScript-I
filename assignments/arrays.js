106b82c51b41af9943dba3d16dc18e9911803364
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-03-27 14:23:29 -0700

    i got to a point

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -73,28 +73,30 @@ console.log('The make and model of the last car is Lincoln and Navagator');
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-for (let i = 0; i < carModels.length; i++){
-    carModels[i];
-    carModels.sort (function(a,b) {return a-b});
-}
-console.log()
+for (let i = 0; i < 50; i++){   
+//console.log(inventory[i].car_model);
 
+}
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-
-
 let carYears = [];
-console.log();
+for (let i =0; i<50; i++){
+    console.log(inventory[i].length);
+}
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-console.log(); 
+for (let i=0; i<50; i++){
+    //console.log(inventory[i].car_year); 
+}
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
-console.log();
+for (let i=0; i<50; i++){
+    //console.log(inventory[i].car_make);
+}
 
 
 
